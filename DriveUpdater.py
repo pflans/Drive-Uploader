79dcffed6568d23789e503ce2515c9257e547338
Patrick Murray <patfmurray@gmail.com>
2012-01-04 17:02:58 -0500

    Update DriveUpdater.py

--- DriveUpdater.py
+++ DriveUpdater.py
@@ -39,6 +39,7 @@ class GUI(Frame):
     
     def createWidgets(self):
         
+        # Only two buttons in the program Select Drive (for uploading) and Upload and Quit (which actually triggers the script to send the file to the ftp
         self.QUIT = Button(self)
         self.QUIT["text"] = "Upload and Quit"
         self.QUIT["command"] =  self.quit
@@ -50,27 +51,32 @@ class GUI(Frame):
         self.OPEN.pack({"side": "top"})
         
 
-    
+# Does not return items that have a period as the first character (used to ignore OS X hidden files)
 def nodot(item): return item[0] != '.'
 
 def DirXML(path):
             global x
         
+            # If x<2 there are no nodes higher so this folder is classified as a "drive"        
             if x<2:
                 result = '<item rel="drive" id="%(number)03d"> \n<content><name> %(pathname)s </name></content> \n' %{"number":x, "pathname":xml_quoteattr(os.path.basename(path))}
+            # Anything with nodes below it and above it is then classified as a "folder"
             else:
                 result = '<item rel="folder" id="%(number)03d"> \n<content><name> %(pathname)s </name></content> \n' %{"number":x, "pathname":xml_quoteattr(os.path.basename(path))}
            
             for item in filter(nodot, os.listdir(path)):
                 itempath = os.path.join(path, item)
+                # If the program comes across another subdirectory it restarts the script and continues down the tree
                 if os.path.isdir(itempath):
                     x = x + 1
                     result += '\n'.join('  ' + line for line in 
                         DirXML(os.path.join(path, item)).split('\n'))
+                # If the program comes across a file it uses the file's path to pull out the file name and give it a "file" classification
                 elif os.path.isfile(itempath):
                     x = x + 1
                     result += '<item rel="file" id="%(number)03d"> \n<content><name>%(pathname)s</name></content>\n</item>\n' %{"number":x, "pathname":xml_quoteattr(item)}    
             
+            #Adds closing markup after the loop
             result += '</item> \n'
             output = result
             return output
@@ -85,11 +91,14 @@ if __name__ == '__main__':
     root.mainloop()
     
     if len(dirname) != 0:
+        # Creates the XML file with the same name as the drive for easy identification
         outputname = (dirname + ".xml")
         a = open(outputname, "w")
         ftp = ftplib.FTP(ftp)
+        # Logs into FTP
         ftp.login(user = username, passwd = password)
         ftp.cwd(xml_directory) 
+        # Writes root markup along with the file tree
         a.write('<root>\n' + DirXML(dirname) + '\n</root>')
         a.close()
         f=open(outputname,'rb')
